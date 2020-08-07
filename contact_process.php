be9578c9d091e847e1a3b3e28ac64b066f484329
prajualsharma <64773854+prajualsharma@users.noreply.github.com>
2020-08-07 13:54:39 +0530

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -35,3 +35,20 @@
     $send = mail($to, $subject, $body, $headers);
 
 ?>
+
+<?php
+if(!empty($_POST["send"])) {
+	$name = $_POST["userName"];
+	$email = $_POST["userEmail"];
+	$subject = $_POST["subject"];
+	$content = $_POST["content"];
+
+	$toEmail = "admin@phppot_samples.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "index.html";
+?>
